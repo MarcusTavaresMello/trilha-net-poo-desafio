e625b17329dadb0bb0deb102fa7c7f53635e21e2
Marcus Mello <marcus-vinicius-tavares@hotmail.com>
2022-09-21 14:18:56 -0300

    Desafio finalizado.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone MeuIphone = new Iphone("16 9999-0001", "XS", "1122334455", 64);
+Nokia MeuNokia = new Nokia("16 9999-0002", "Nk 5.4", "0011001100", 128);
+
+MeuIphone.InstalarAplicativo("< DIO >");
+MeuNokia.InstalarAplicativo("< LinkedIn >");
